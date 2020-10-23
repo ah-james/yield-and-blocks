834c9b8c170df1be63c6436876051b5c401a5c72
ah-james <11ahj11@gmail.com>
2020-10-23 16:33:13 -0400

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
-def hello_t
-
+def hello_t(array)
+    if block_given?
+    i = 0 # creating a variable with 0 as the value
+    while i < array.length
+        yield array[i] # yields current length of array before adding 1
+        i += 1 # as long as i is less than the length of the array, add 1 to i and run again
+    end
+    array
+    else
+        puts "Hey! No block was given!"
+    end
 end
 
 # call your method here!
-
